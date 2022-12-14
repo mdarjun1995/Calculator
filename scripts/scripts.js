dda6e36108eda3b9c9d0378b6278dd3d2f732e0f
mdarjun1995 <99963378+mdarjun1995@users.noreply.github.com>
2022-12-14 23:07:48 +0530

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,25 @@
+let string = "";
+        let buttons = document.querySelectorAll('.button');
+        Array.from(buttons).forEach((button)=>{
+            button.addEventListener('click', (e)=>{
+                if(e.target.innerHTML == '='){
+                    string = eval(string);
+                    document.querySelector('input').value = string;
+                    }
+                else if(e.target.innerHTML == 'C'){
+                    string = ""
+                    document.querySelector('input').value = string;
+                    }
+                // else if(e.target.innerHTML == 'D'){
+                //     string = string.substring(0, string.length() - 1) ;
+                //     console.log(string) ; 
+                //     // (string == null || string.length() == 0) ? null :
+                //     document.querySelector('input').value = string;
+                // }
+                else{ 
+                    console.log(e.target)
+                    string = string + e.target.innerHTML;
+                    document.querySelector('input').value = string;
+                    }
+                })
+            })
